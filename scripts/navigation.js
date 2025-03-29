cf4fcd7806fc0e8056cb7cfc052552d38aae1029
Julirey <132237425+Julirey@users.noreply.github.com>
2025-03-29 14:01:19 -0400

    Add animated navigation

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,6 @@
 // Hamburguer menu 
 const hamButton = document.querySelector("#menu");
-const navigation = document.querySelector(".navigation");
+const navigation = document.querySelector("#animate-me");
 
 hamButton.addEventListener("click", () => {
   navigation.classList.toggle("open");
